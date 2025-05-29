29752a4689573d71faf4cd06a2597992e84f4979
Saugat Khatiwada <saugatkhatiwada80@gmail.com>
2025-05-29 22:25:43 +0545

    removed unwanted depencencies and files

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,15 @@
-import express from 'express'
-import authRoutes from './routes/routes.js'
-import cors from 'cors'
+import express from "express";
 
+import cors from "cors";
 
-const app=express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.get('/api/auth/chat', (req, res) => {
-    res.json({ message: 'Hello from server' });
+app.get("/api/auth/chat", (req, res) => {
+  res.json({ message: "Hello from server" });
 });
 
-
-
-
-app.listen(5000,()=>{
-    console.log('Server is running on port 5000');
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
-
-
